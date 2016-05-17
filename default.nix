c34a39ffd2f8e7dca52d5016aa84421884d45e8a
Florent Becker <florent.becker@ens-lyon.org>
2016-05-17 15:57:53 +0000

    Build pijul against stable rust (1.6.0)
    Ignore-this: 64b8eb1df2a05f3c09fba6390fc568e8
    
    darcs-hash:4ea5d8e7a599d3fef39930829592b9fe1b66a20f

--- default.nix
+++ default.nix
@@ -5,6 +5,6 @@ stdenv.mkDerivation rec {
   version = "0.0";
   src = ./.;
 
-  buildInputs = [ rustUnstable.rustc rustUnstable.cargo lmdb openssl libssh zlib pkgconfig ];
+  buildInputs = [ rustPlatform.rustc rustPlatform.cargo lmdb openssl libssh zlib pkgconfig rustfmt ];
   
 }
